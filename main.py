2803127b595514af199791ab5cfb5f6a824d29a3
dmarek03 <domarek@student.agh.edu.pl>
2024-12-30 16:51:03 +0100

    Added main

---
+++ main.py
@@ -0,0 +1,48 @@
+from topsis_app.comparison_matrix import ComparisonMatrix
+from topsis_app.topsis_algorithm import TopsisCalculator
+
+
+def main() -> None:
+    products = ["A", "B", "C", "D", "E"]
+    categories = ["RAM", "Memory", "DisplaySize", "Battery", "Price"]
+    comparison_data = [
+        [4, 128, 6.5, 3500, 15000],
+        [6, 64, 6.4, 3800, 16000],
+        [6, 128, 6.8, 4200, 19000],
+        [8, 256, 7, 5000, 25000],
+        [3, 64, 6.2, 4000, 14000],
+    ]
+
+    weights = [1.0, 1.0, 1.0, 1.0, 1.0]
+
+    criteria_impact = ["+", "+", "+", "+", "-"]
+
+    comparison_matrix = ComparisonMatrix(
+        products=products,
+        categories=categories,
+        data_matrix=comparison_data,
+        criteria_name="Model",
+    )
+    print(comparison_matrix)
+
+    topsis = TopsisCalculator(
+        comparison_matrix=comparison_matrix,
+        weight_matrix=weights,
+        criteria_impact=criteria_impact,
+    )
+
+    topsis.normalize_data_matrix()
+    print(*topsis.comparison_matrix.data_matrix, sep="\n")
+
+    p_dist, n_dist = topsis.calculate_distances()
+    print(f"{p_dist=}")
+    print(f"{n_dist=}")
+
+    score = topsis.calculate_rank()
+    print(f"{score=}")
+
+    print(topsis.comparison_matrix)
+
+
+if __name__ == "__main__":
+    main()
